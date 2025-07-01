3447f3660bb9e984e3aa42418b726762252581f7
Md. Salauddin <salauddinahmed555@gmail.com>
2025-07-01 13:07:46 +0600

    created new script for soving EOL to LF from CRLF

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloController } from './hello/hello.controller';
+import { HelloModule } from './hello/hello.module';
 import { HelloService } from './hello/hello.service';
 import { HelloModule } from './hello/hello.module';
+import { HelloController } from './hello/hello.controller';
 
 @Module({
   imports: [HelloModule],
