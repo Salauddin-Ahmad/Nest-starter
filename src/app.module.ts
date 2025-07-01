5da646ab996dc697cdec4178388e9b33a5bb2f17
Md. Salauddin <salauddinahmed555@gmail.com>
2025-07-01 13:47:11 +0600

    generate prisma not full set up yet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloController } from './hello/hello.controller';
 import { HelloModule } from './hello/hello.module';
+import { HelloController } from './hello/hello.controller';
 import { HelloService } from './hello/hello.service';
-import { HelloModule } from './hello/hello.module';
-import { HelloController } from './hello/hello.controller';
 
 @Module({
   imports: [HelloModule],
