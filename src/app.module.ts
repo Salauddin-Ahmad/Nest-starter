49518b15e3451a6e6922d30e4d724bcb80f2e782
Md. Salauddin <salauddinahmed555@gmail.com>
2025-07-04 12:08:11 +0600

    generated user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { HelloController } from './hello/hello.controller';
 import { HelloService } from './hello/hello.service';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HelloModule],
-  controllers: [AppController, HelloController],
-  providers: [AppService, HelloService],
+  imports: [HelloModule, UserModule],
+  controllers: [AppController, HelloController, UserController],
+  providers: [AppService, HelloService, UserService],
 })
 export class AppModule {}
